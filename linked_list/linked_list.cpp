8029f76b1ffbf844140a45b6748ec5f97277ad23
root <root@santoryu1118.localdomain>
2021-09-04 16:42:43 +0900

    9월 4일 중간

--- linked_list/linked_list.cpp
+++ linked_list/linked_list.cpp
@@ -2,17 +2,34 @@
 
 // function definition
 
-Node* create_node(){
+Node* create_node(int value){
     Node* new_node = (Node*)malloc(sizeof(Node));
+    new_node->data = value;
     return new_node;
 }
 
-void add_node(Node* curr_node){
-    Node* new_node = create_node();
-    curr_node->next = new_node;
+// 필요한가?
+Node* find_node(Node* head, int value){
+    for (Node* p = head; p!= NULL; p = p->next){
+        if (p->data ==value){
+            return p;
+        }
+    }
+    return NULL;
 }
 
-void delete_node(Node* curr_node){
-    void *address = curr_node;
+// 해당 position에 새로운 node insert
+void insert_node(Node* head, int value, unsigned pos){
+    Node* new_node = create_node(value);
+    Node* p = head;
+    for (unsigned idx = 0; idx != pos && p->next != NULL; idx++, p = p->next);
+    new_node->next = p->next;
+    p->next = new_node;
+}
+
+// delete 할 시에는 포인터 두개잡고 prev node도 들고 있어야됨 
+void delete_node(Node* head, int value){
+    for (Node* p = head; p->data != value && p!= NULL; p = p->next);
+
 }
 
